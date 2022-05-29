a92f64aa17e813a4ec799ab478b7ec41140d7c87
Zoltan Csilleri <csilleri.zoltan@codebuild.hu>
2022-05-29 09:48:33 +0200

    wip

---
+++ src/libs/api-helper.ts
@@ -0,0 +1,92 @@
+import axios from 'axios';
+
+export class Repository {
+  public port: string;
+  public baseUrl = process.env.BASE_URL;
+  public appPath: string;
+
+  // TODO -> port is there to define which application we are targeting
+  public constructor(port: string) {
+    this.port = port;
+    this.appPath = `${this.baseUrl}${port}`;
+  }
+
+  public async get(href: string) {
+    const path = `${this.appPath}/${href}`;
+    return await new Promise((resolve, reject) => {
+      try {
+        const response = (axios as any).get(path, {
+          headers: { 'Accept': 'application/json', },
+          params: {}
+        });
+        resolve(response?.data);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+
+  public async post(href: string) {
+    const path = `${this.appPath}/${href}`;
+
+    return await new Promise((resolve, reject) => {
+      try {
+        const response = (axios as any).get(path, {
+          headers: { 'Accept': 'application/json', },
+          params: {}
+        });
+        resolve(response?.data);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+
+  public async post(href: string) {
+    const path = `${this.appPath}/${href}`;
+
+    return await new Promise((resolve, reject) => {
+      try {
+        const response = (axios as any).post(path, {
+          headers: { 'Accept': 'application/json', },
+          params: {}
+        });
+        resolve(response?.data);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+
+  public async put(href: string) {
+    const path = `${this.appPath}/${href}`;
+
+    return await new Promise((resolve, reject) => {
+      try {
+        const response = (axios as any).put(path, {
+          headers: { 'Accept': 'application/json', },
+          params: {}
+        });
+        resolve(response?.data);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+
+  public async delete(href: string) {
+    const path = `${this.appPath}/${href}`;
+
+    return await new Promise((resolve, reject) => {
+      try {
+        const response = (axios as any).delete(path, {
+          headers: { 'Accept': 'application/json', },
+          params: {}
+        });
+        resolve(response?.data);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+}
