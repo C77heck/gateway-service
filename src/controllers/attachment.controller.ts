76b5daabf78749ebc4b13d0e2d0a9941617a00d5
Zoltan Csilleri <csilleri.zoltan@codebuild.hu>
2022-05-30 19:10:12 +0200

    wip

--- src/controllers/attachment.controller.ts
+++ src/controllers/attachment.controller.ts
@@ -6,21 +6,22 @@ import { attachmentHandler } from './handlers/attachment.handler';
 import { ReqObject } from './libs/req.object';
 import { getIsValid } from './libs/validity-check';
 
+const repository = new Repository(process.env.ATTACHMENT_PORT);
+
 // TODO -> we need to test all the endpoints and see how we could manage them based around predefined requests.
 // we will propably have to deal with the authentication on gateway side for this to work. and allow
 // the underlying api services to function without that constraint.
 export const attachmentController = async (req: express.Request, res: express.Response, next: NextFunction) => {
   try {
-    const { originalUrl, method } = new ReqObject(req);
+    const { originalUrl, method, params, query } = new ReqObject(req);
 
     if (getIsValid(originalUrl, method, attachmentHandler)) {
       res.send();
     }
 
-    const repository = new Repository(process.env.ATTACHMENT_PORT);
-
-    repository[method]()
-    res.json({ originalUrl, method });
+    const response = await repository.fetch({ method, baseURL: originalUrl, params: query });
+    console.log(response);
+    res.end();
   } catch (e) {
     return next(new HttpError(ERROR_MESSAGES.GENERIC));
   }
